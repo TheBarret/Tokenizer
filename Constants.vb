58403439204eba73b3d69594394a413967319217
TheBarret <format.e.drive@gmail.com>
2017-01-23 09:35:44 +0100

    Added config serialization features

--- Constants.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Class Constants
-    Public Shared Signature As Byte() = New Byte(1) {&HAF, &HAB}
-    Public Shared DESCRIBER_LINE As String = "^\bdefine\b\s+(?<name>[a-z0-9_.]*?)\s+(?<pattern>"".*?""?)\;"
-End Class
--- Provider.vb
+++ Provider.vb
@@ -5,38 +5,18 @@ Imports System.Runtime.InteropServices
 <Serializable>
 Public NotInheritable Class Provider
     Inherits List(Of Describer)
-    Sub New()
-        Me.Encoder = Encoding.UTF8
-    End Sub
-    Sub New(Encoder As Encoding)
-        Me.Encoder = Encoder
-    End Sub
+    Private Signature() As Byte = New Byte(1) {&HFF, &HAF}
+    Private Pattern As String = "^\bdefine\b\s+(?<name>[a-z0-9_.]*?)\s+(?<pattern>"".*?""?)\;"
     Public Function FromSerializedFile(Filename As String) As Provider
-        If (File.Exists(Filename)) Then
-            Using fs As New FileStream(Filename, FileMode.Open, FileAccess.Read)
-                Dim buffer() As Byte = New Byte(Convert.ToInt32(fs.Length - 1)) {}
-                fs.Read(buffer, 0, buffer.Length)
-                If (buffer.BeginsWith(Constants.Signature)) Then
-                    Dim checksum() As Byte = buffer.Peek(Constants.Signature.Length, 4)
-                    buffer = buffer.NibbleTop(Constants.Signature.Length + checksum.Length)
-                    If (checksum.SequenceEqual(buffer.ToCrc32)) Then
-                        Dim obj As Object = FromByte.Convert(buffer.Decompress)
-                        If (TypeOf obj Is Provider) Then
-                            Me.AddRange(CType(obj, Provider))
-                        End If
-                        Return Me
-                    End If
-                End If
-            End Using
+        Dim provider As Provider = Nothing
+        If (New HeaderCreator.Reader().IntegrityCheck(Of Provider)(Filename, Me.Signature, provider)) Then
+            Me.AddRange(provider)
         End If
-        Throw New Exception("Invalid file")
+        Return Me
     End Function
-    Public Sub ToSerializedFile(Filename As String)
-        Using fs As New FileStream(Filename, FileMode.Create, FileAccess.Write)
-            Dim buffer() As Byte = FromObject.Convert(Me).Compress.InsertAfter(Function(arg) arg.ToCrc32(), Constants.Signature, 0)
-            fs.Write(buffer, 0, buffer.Length)
-        End Using
-    End Sub
+    Public Function ToSerializedFile(Filename As String) As Boolean
+        Return New HeaderCreator.Writer().Build(Me, Me.Signature, Filename)
+    End Function
     Public Function FromFile(Filename As String) As Provider
         If (File.Exists(Filename)) Then
             Return Me.FromStream(New FileStream(Filename, FileMode.Open, FileAccess.Read))
@@ -46,10 +26,10 @@ Public NotInheritable Class Provider
     Public Function FromStream(stream As Stream) As Provider
         Dim buffer() As Byte = New Byte(Convert.ToInt32(stream.Length)) {}
         stream.Read(buffer, 0, buffer.Length)
-        Return Me.FromString(Me.Encoder.GetString(buffer))
+        Return Me.FromString(Encoding.UTF8.GetString(buffer))
     End Function
     Public Function FromString(str As String) As Provider
-        Dim rx As New Regex(Constants.DESCRIBER_LINE, RegexOptions.IgnoreCase Or RegexOptions.Multiline)
+        Dim rx As New Regex(Me.Pattern, RegexOptions.IgnoreCase Or RegexOptions.Multiline)
         For Each m As Match In rx.Matches(str)
             If (m.Success) Then
                 Me.Add(New Describer(m.Groups("name").Value, m.Groups("pattern").Value))
@@ -57,5 +37,4 @@ Public NotInheritable Class Provider
         Next
         Return Me
     End Function
-    Public Property Encoder As Encoding
 End Class
